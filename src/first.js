aee0a443dc309895984d17641680d3f36f7cadb1
pavetok <p.vetokhin@gmail.com>
2016-06-07 21:15:31 +0300

    initial commit

---
+++ src/first.js
@@ -0,0 +1,11 @@
+// @flow
+
+function name() {
+  console.log('i am here');
+}
+name();
+
+function foo(x) {
+  return x * 10;
+}
+foo(1);
---
+++ test/test-first.js
@@ -0,0 +1,12 @@
+const expect = require('chai').expect;
+
+
+describe('test', () => {
+  it('first', () => {
+    expect(1).to.equal(1);
+  });
+
+  it('second', () => {
+    expect(1).to.equal(1);
+  });
+});
