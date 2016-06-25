48456f82c2f788f17da836c54e8627aae2b33694
pavetok <p.vetokhin@gmail.com>
2016-06-25 19:23:35 +0300

    first passed test

--- src/drundel.js
+++ src/drundel.js
@@ -1,7 +1,18 @@
-function drundel(spec) {
+const EventEmitter = require('events');
+
+const eventBus = new EventEmitter();
+
+function drundel(events) {
+  /**
+   * Принимает объект с ожидаемыми событиями и значениями.
+   * При наступлении события инкрементирует значение.
+   */
   const that = Object.create({});
-  Object.assign(that, spec);
+  Object.assign(that, events);
+  Object.keys(events).forEach(
+    e => eventBus.on(e, () => that[e]++)
+  );
   return that;
 }
 
-module.exports = drundel;
+module.exports = { drundel, eventBus };
--- src/test-drundel.js
+++ src/test-drundel.js
@@ -1,9 +1,21 @@
 const expect = require('chai').expect;
-const drundel = require('./drundel');
+const drundel = require('./drundel').drundel;
+const eventBus = require('./drundel').eventBus;
 
 describe('Drundel', () => {
   it('correctly initialized', () => {
     const d = drundel({ a: 1 });
-    expect(d).to.have.property('a');
+    expect(d).to.have.property('a', 1);
+  });
+
+  it('increments value on event', () => {
+    // given
+    const events = { a: 1, b: 2 };
+    // and
+    const d = drundel(events);
+    // when
+    eventBus.emit('a');
+    // then
+    expect(d).to.have.property('a', 2);
   });
 });
